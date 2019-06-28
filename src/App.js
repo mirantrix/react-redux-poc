c0d9c5f6e994d81925b418c270f3aa4e026f7466
devLife <mirantrix80@gmail.com>
2019-06-27 20:06:02 -0500

    [Feature]: Add React Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React-Redux Setup</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>React-Redux Setup</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 const mapStateProps = state => {
